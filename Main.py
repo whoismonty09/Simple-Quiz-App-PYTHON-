ea63d11bd2d4dfd3772b81f452b3bf46e08265b9
Mayank malviya <mayankmalviya440@gmail.com>
2026-02-01 08:33:53 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,28 @@
+quiz = {
+    "What is capital of India?":
+    "Delhi",
+    "Which language is used for web development?":
+    "Python",
+    "What is 5 + 7?": "12",
+    "Who developed Python?": "Guido van Rossum"
+}
+
+score = 0
+print("Welcome to the Quiz developed by Monty")
+
+for question, answer in quiz.items():
+    print("\n" + question)
+    user_answer = input("Your answer: ")
+
+    if user_answer.strip().lower() == answer.lower():
+        print("Correct")
+        score += 1
+    else:
+        print("Wrong")
+        print("Correct answer is :", answer) 
+
+print("\n Quiz Completed")
+print(f"Your Score : {score}/{len(quiz)}")
+           
+
 
